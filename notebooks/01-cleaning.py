5f788dbf57232b472f4cf41058e28582d3bce745
Khadidja <mk_foudili@esi.dz>
2025-04-23 16:25:53 +0100

    automatically filling missing cells

--- notebooks/01-cleaning.py
+++ notebooks/01-cleaning.py
@@ -1,5 +1,6 @@
 # importing libraries
 import pandas as pd
+import numpy as np
 import os
 
 # load csv files
@@ -19,8 +20,43 @@ for csv_file in os.listdir(folder):
 # merge all csv files
 df = pd.concat(df_list, ignore_index=True)
 
+
+# some functions to explore the data frame
+
 # print(df.head())
 # print(df.columns)
 # print(df.shape)
 # print(df.info())
 # print(df.describe())
+
+# how many missing values de we have in each column ?
+# print(df.isnull().sum())
+
+# pourcentage of null in each column
+# total_missing = df.isnull().sum()
+
+# total_cells = df.size
+# # or: total_cells = np.product(df.shape)
+
+# print((total_missing/total_cells) * 100)
+
+# handeling missing values
+# 01- discount
+df['discount'] = df['discount'].fillna(0)
+
+# 02- price
+mod = df.groupby('category')['price'].agg(lambda x: x.mode().iloc[0])
+df['price'] = df['discount'].fillna(df['category'].map(mod))
+
+# 03- rank title / rank sub
+df['rank-title'] = df['rank-title'].fillna(0)
+df['rank-sub'] = df['rank-sub'].fillna(0)
+
+# 04- product color
+df['color-count'] = df['color-count'].fillna(0)
+
+# 06- selling proposition
+df['selling_proposition'] = df['selling_proposition'].fillna(0)
+
+# print data after handeling null values
+print(df.isnull().sum())
