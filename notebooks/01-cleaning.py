7206a6e4cc2fcf2a70569fcf08e8ebbf9e3b186d
Khadidja <mk_foudili@esi.dz>
2025-04-23 13:06:49 +0100

    load a csv file into a pandas data frame

---
+++ notebooks/01-cleaning.py
@@ -0,0 +1,23 @@
+# importing libraries
+import pandas as pd
+import os
+
+# load csv files
+folder = './data'
+df_list = []
+df = pd.DataFrame()
+
+
+for csv_file in os.listdir(folder):
+    file = os.path.join(folder,csv_file)
+    df = pd.read_csv(file)
+    # every csv file is a product category so add this as a column
+    category = csv_file.split('-')[-2]
+    df['category'] = category
+    df_list.append(df)
+
+# merge all csv files
+df = pd.concat(df_list, ignore_index=True)
+
+# print(df.head())
+# print(df.columns)
