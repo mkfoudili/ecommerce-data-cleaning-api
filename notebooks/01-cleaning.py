b729892b928e3760e24e9a3e4fd7f6ca5cc3384e
Khadidja <mk_foudili@esi.dz>
2025-04-23 19:30:21 +0100

    standarization of cells values

--- notebooks/01-cleaning.py
+++ notebooks/01-cleaning.py
@@ -1,6 +1,7 @@
 # importing libraries
 import pandas as pd
 import numpy as np
+import re
 import os
 
 # load csv files
@@ -46,17 +47,17 @@ df['discount'] = df['discount'].fillna(0)
 
 # 02- price
 mod = df.groupby('category')['price'].agg(lambda x: x.mode().iloc[0])
-df['price'] = df['discount'].fillna(df['category'].map(mod))
+df['price'] = df['price'].fillna(df['category'].map(mod))
 
 # 03- rank title / rank sub
-df['rank-title'] = df['rank-title'].fillna(0)
-df['rank-sub'] = df['rank-sub'].fillna(0)
+df['rank-title'] = df['rank-title'].fillna('')
+df['rank-sub'] = df['rank-sub'].fillna('')
 
 # 04- product color
 df['color-count'] = df['color-count'].fillna(0)
 
 # 06- selling proposition
-df['selling_proposition'] = df['selling_proposition'].fillna(0)
+df['selling_proposition'] = df['selling_proposition'].fillna('')
 
 # 07- product title
 df['goods-title-link--jump'] = df['goods-title-link--jump'].fillna('')
@@ -75,4 +76,49 @@ df = df.drop('blackfridaybelts-content', axis=1)
 df = df.drop('product-locatelabels-img src', axis=1)
 
 # print data after handeling null values
-print(df.isnull().sum())
+# print(df.isnull().sum())
+
+# satndarisation
+# 01- clean column names
+df.columns = df.columns.str.replace('_','-')
+df.columns = df.columns.str.replace(' ','-')
+
+# 02- lower / upper case
+df['goods-title'] = df['goods-title'].str.title()
+df['category'] = df['category'].str.title()
+df['category'] = df['category'].str.replace('_',' ')
+df['selling-proposition'] = df['selling-proposition'].str.title()
+df['rank-title'] = df['rank-title'].str.title()
+df['rank-sub'] = df['rank-sub'].str.title()
+
+# 03- trim white spaces
+for column in df.columns:
+    if df[column].dtype == object:
+        df[column] = df[column].str.split().str.join(' ')
+
+# 04- trating inconsistent data entries
+# use value counts on each column to find inconsistencies
+
+# print(df['category'].value_counts())
+# print(df['color-count'].value_counts())
+# print(df['discount'].value_counts())
+# print(df['goods-title'].value_counts())
+# print(df['price'].value_counts())
+# print(df['rank-sub'].value_counts())
+# print(df['rank-title'].value_counts())
+# print(df['selling-proposition'].value_counts())
+
+ranks = []
+for rank in df['rank-title']:
+    if 'Sellers' in rank:
+        rank = rank.replace('Sellers','Seller')    
+    ranks.append(rank)
+
+df['rank-title'] = ranks
+
+# verify the format of this column
+pattern = r"^\d+(\.\d+)?K?\+\sSold Recently$"
+
+for prop in df['selling-proposition']:
+    if not re.match(pattern, prop) and prop != '':
+        # print(prop)
